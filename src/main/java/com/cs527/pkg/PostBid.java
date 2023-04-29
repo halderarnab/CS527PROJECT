979b20e80e4879e6e81a29a00e93df192d29ceed
joshi-k <kautilyajoshi97@gmail.com>
2023-04-29 02:33:50 -0400

    added java to post bid

--- src/main/java/com/cs527/pkg/PostBid.java
+++ src/main/java/com/cs527/pkg/PostBid.java
@@ -20,16 +20,26 @@ public class PostBid extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response){
-		//java.util.logging.Logger logger =  java.util.logging.Logger.getLogger(this.getClass().getName());
-		//FileHandler fileHandler = new FileHandler("status.log");
-		//logger.addHandler(fileHandler);
 		
-		String userId = null;
-		String auctionId = null;
+		int userId = 0;
+		int auctionId = 0;
+		int autoBid = 0;
 		HttpSession session = request.getSession();
-		userId = (String) session.getAttribute("userId");
-		auctionId = (String) session.getAttribute("auctionId");
-
+		userId = (int) session.getAttribute("userId");
+		auctionId = (int) session.getAttribute("auctionId");
+		int bidPrice = Integer.parseInt(request.getParameter("bidPrice")); 
+		String autoBidParam = request.getParameter("autoBid");
+		int MaxBid = 0;
+		
+		try {
+			if (autoBidParam != null || autoBidParam != "") {
+				autoBid = Integer.parseInt(autoBidParam);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
 		//Get the database connection
 		ApplicationDB db = new ApplicationDB();	
 		Connection con = null;
@@ -42,28 +52,21 @@ public class PostBid extends HttpServlet {
 		
 		//Fetching user_id from user table
 		try {
-			String bidPrice = request.getParameter("bidPrice");
-			String autoBid = request.getParameter("autoBid");
-			
 			ResultSet rs = null;
 
 			//Make an insert statement for the Sells table:
-			String fetchUserId = "SELECT user_id FROM user WHERE email=?";
+			String fetchMaxBidId = "SELECT MAX(bid_id) FROM bid;";
 			//Create a Prepared SQL statement allowing you to introduce the parameters of the query
-			PreparedStatement ps = con.prepareStatement(fetchUserId);
+			PreparedStatement ps = con.prepareStatement(fetchMaxBidId);
 
 			//Add parameters of the query. Start with 1, the 0-parameter is the INSERT statement itself
-			ps.setString(1, email);
 			rs = ps.executeQuery();
 			
 			if (rs.next()) {
-				//HttpSession session = request.getSession();
-				//session.setAttribute("email", email);
-				userId = rs.getString("user_id");
-				//response.sendRedirect("welcome.jsp");
+				MaxBid = rs.getInt(1);
 			} else {
-				request.setAttribute("message", "User does not exist!");
-				RequestDispatcher rd = request.getRequestDispatcher("updateinfo.jsp");
+				request.setAttribute("message", "Error in getting max bid_id!");
+				RequestDispatcher rd = request.getRequestDispatcher("AuctionIndividual.jsp");
 				rd.forward(request, response);
 				}
 			//con.close();
@@ -78,25 +81,12 @@ public class PostBid extends HttpServlet {
 			e.printStackTrace();
 		}
 		
-		String bankAccountNo = null;
-		
-		bankAccountNo = request.getParameter("bankacc");
+		MaxBid += 1; 
 		
 		//System.out.println("bankAccount '" + bankAccountNo+ "'");
 		//System.out.println("bankAccount Length" + bankAccountNo.length());
 		
-		try {
-			if (bankAccountNo.length()!=12) {
-				request.setAttribute("message", "Bank Account needs to be 12 characters long!");
-				RequestDispatcher rd = request.getRequestDispatcher("updateinfo.jsp");
-				rd.forward(request, response);
-			}
-		} catch (ServletException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		String updateBankAccount = "UPDATE end_user SET bank_account_no=? WHERE user_id=?;";
+		String updateBankAccount = "INSERT INTO bid(bid_id, price, timestamp, upper_limit, auction_id, user_id) values(?,?,NOW(),?,?,?)";
 
 		//Create a Prepared SQL statement allowing you to introduce the parameters of the query
 		try {
@@ -104,18 +94,19 @@ public class PostBid extends HttpServlet {
 			ps = con.prepareStatement(updateBankAccount);
 
 			//Add parameters of the query. Start with 1, the 0-parameter is the INSERT statement itself
-			ps.setString(1, bankAccountNo);
-			ps.setString(2, userId);
+			ps.setInt(1, MaxBid);
+			ps.setInt(2, bidPrice);
+			ps.setInt(3, autoBid);
+			ps.setInt(4, auctionId);
+			ps.setInt(5, userId);
 			ps.executeUpdate();
 			//con.commit();
 			//System.out.println("updated Bank Account to " + bankAccountNo);
-			request.getSession().setAttribute("bankAccountNo", bankAccountNo);
 			con.close();
-			request.setAttribute("message", "Bank Account Updated");
+			request.setAttribute("message", "Bid Posted");
 			//response.sendRedirect("updateInfo.jsp");
-			RequestDispatcher rd = request.getRequestDispatcher("updateInfo.jsp");
+			RequestDispatcher rd = request.getRequestDispatcher("AuctionIndividual.jsp");
 			rd.forward(request, response);
-			//rs = ps.executeQuery();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
